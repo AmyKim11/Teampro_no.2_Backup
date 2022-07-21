1566b02e05c0d5c597121cbd32e2f1339695fd80
Amykim11 <kcass041@gmail.com>
2022-07-21 14:20:26 +0900

    main-sub nav css fix and service part css add

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 // html dom 이 다 로딩된 후 실행된다.    
 $(document).ready(function(){
-   // menu 클래스 바로 하위에 있는 a 태그를 클릭했을때        
+   // menu 클래스 바로 하위에 있는 태그를 클릭했을때        
    $(".hamburger-menu").click(function(){
-      let submenu = $(this).next(".hide-nav-texts");
+      let submenu = $(".hide-nav-texts");
    // submenu 가 화면상에 보일때는 위로 보드랍게 접고 아니면 아래로 보드랍게 펼치기            
    if( submenu.is(":visible") ){submenu.slideUp();}
    else{submenu.slideDown();}
